c6d3b4395191dd2312d9a08a49c11d404b89577f
elisa.zyj <elisa.zyj@link8tech.tw>
2021-09-15 18:29:34 +0800

    fix([update]): 更新 header、nav

--- react_q2/src/components/layout/Navigation.js
+++ react_q2/src/components/layout/Navigation.js
@@ -1,6 +1,14 @@
+import { NavLink } from 'react-router-dom'
+import '@/App.css'
+
 const Navigation = () => {
   return (
-    <div className="">Navigation</div>
+    <div className="relative h-screen w-36 border-r border-gray-500">
+      <div className="absolute inset-0">
+        <NavLink className='block py-5' to="/personalManagement">個人資料管理</NavLink>
+        <NavLink className='block py-5' to="/memberManagement" activeClassName="selected">會員管理</NavLink>
+      </div>
+    </div>
   )
 }
 
