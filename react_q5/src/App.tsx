fc6e652fc24ea411d738ecc94e4a09d8383089b9
elisa <simplelife730@gmail.com>
2021-10-25 23:42:27 +0800

    feat([create]): 畫面切版

--- react_q5/src/App.tsx
+++ react_q5/src/App.tsx
@@ -1,26 +1,60 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { FaRegThumbsUp, FaRegThumbsDown, FaRegClock } from "react-icons/fa";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="flex h-screen w-screen">
+            <div className="flex items-center justify-center m-auto h-5/6 w-5/6 bg-blue-900 text-center">
+                <div className="flex flex-col bg-blue-500 w-3/12 h-full">
+                    <div className="bg-blue-100 flex items-center justify-center h-1/6 mb-4 text-6xl">
+                        <FaRegClock className='mr-2' />
+                        <div className='mb-2'>60:00</div>
+                    </div>
+                    <div className="bg-blue-200 flex-1">
+                        <ul>
+                            <li className="bg-blue-50 flex items-center">
+                                <div className="relative w-1/4 h-20">
+                                    <img src="http://fakeimg.pl/60/555555/FFA500/?text=F" alt="" className="rounded-full m-3"/>
+                                    <div className="absolute top-2 left-2 flex items-center justify-center border-black border-2 rounded-full w-5 h-5">
+                                        <p>1</p>
+                                    </div>
+                                    <div className="absolute right-0 bottom-2 w-1 h-1 bg-green-500 rounded-full"></div>
+                                </div>
+                                <div className="ml-3">
+                                    <div className="flex items-center">
+                                        <p>frank (you)</p>
+                                        <p>12分</p>
+                                    </div>
+                                    <div className="flex items-center mt-2">
+                                        <FaRegThumbsUp className='mr-2 text-lg' />
+                                        <FaRegThumbsDown className='mr-2 text-lg' />
+                                    </div>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <div className="flex flex-col bg-pink-400 w-5/12 h-full mx-3">
+                    <div className="bg-blue-100 h-1/3 mb-4">
+                        題目名稱 & 簡述
+                    </div>
+                    <div className="bg-blue-200 flex-1">
+                        題目詳情
+                    </div>
+                </div>
+                <div className="relative flex justify-center bg-green-500 w-4/12 h-full">
+                    <div>
+                        訊息及系統通知
+                    </div>
+                    <div className="absolute flex justify-center bottom-0 mb-3 w-full">
+                        <input className="w-4/6" type="text"/>
+                        <button className="bg-blue-300 w-14 h-6 rounded-sm ml-3">送出</button>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
 }
 
 export default App;
